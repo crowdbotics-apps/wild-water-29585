ef203977d7da22b43957a874673187e2422053a3
Crowdbotics <team@crowdbotics.com>
2021-08-06 23:17:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wild_water_29585Identifier",
+  url: "https://wild_water_29585Identifier.botics.co",
+  api: "https://wild_water_29585Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
